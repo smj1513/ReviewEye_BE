b0697bc9fd1cea9d532b0af16205b82958df0da4
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-09-06 17:44:56 +0900

    ㅇㅇ

--- src/main/java/spring/changyong/search/config/HttpClientConfigImpl.java
+++ src/main/java/spring/changyong/search/config/HttpClientConfigImpl.java
@@ -1,15 +1,10 @@
 package spring.changyong.search.config;
 
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.SSLContexts;
 import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.net.ssl.SSLContext;
@@ -35,13 +30,9 @@ public class HttpClientConfigImpl implements HttpClientConfigCallback {
 	@Override
 	public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
 		try {
-			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-			UsernamePasswordCredentials usernamePasswordCredentials = new UsernamePasswordCredentials(username, password);
-			credentialsProvider.setCredentials(AuthScope.ANY, usernamePasswordCredentials);
-			httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-
 			File trustLocationFile = new File(trustStore);
 			SSLContextBuilder sslContextBuilder = SSLContexts.custom()
+					//	.loadTrustMaterial((chain, authType) -> true); // 모든 인증서를 신뢰합니다.
 					.loadTrustMaterial(trustLocationFile, trustStorePassword.toCharArray());
 			SSLContext sslContext = sslContextBuilder.build();
 			httpAsyncClientBuilder.setSSLContext(sslContext);

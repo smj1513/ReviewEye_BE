a76556d8edc1876de95c1d652928fb832f730b4c
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-09-08 16:41:12 +0900

    [Feat] 상품 상세정보 기능 추가

--- src/main/java/spring/changyong/docs/controller/ProductDocsController.java
+++ src/main/java/spring/changyong/docs/controller/ProductDocsController.java
@@ -1,14 +1,32 @@
 package spring.changyong.docs.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.web.bind.annotation.PathVariable;
 import spring.changyong.common.api.response.CommonResponse;
 import spring.changyong.product.api.response.ProductResponse;
 
-@Tag(name = "Product", description = "상품 API")
+@Tag(name = "상품 API", description = "상품 API")
 public interface ProductDocsController {
-	CommonResponse<ProductResponse.Detail> getProductDetail(@PathVariable Integer id);
-	CommonResponse<ProductResponse.PositiveKeyword> getPositiveKeyword(@PathVariable Integer id);
-	CommonResponse<ProductResponse.NegativeKeyword> getNegativeKeyword(@PathVariable Integer id);
+
+	@Operation(summary = "상품 상세 조회", description = "상품 상세 조회")
+	@ApiResponse(responseCode = "200", description = "성공")
+	CommonResponse<ProductResponse.Detail> getProductDetail(
+			@Parameter(description = "상품 ID")
+			@PathVariable Integer id);
+
+	@Operation(summary = "상품 긍정 키워드 조회", description = "상품 긍정 키워드 조회")
+	@ApiResponse(responseCode = "200", description = "성공")
+	CommonResponse<ProductResponse.PositiveKeyword> getPositiveKeyword(
+			@Parameter(description = "상품 ID")
+			@PathVariable Integer id);
+
+	@Operation(summary = "상품 부정 키워드 조회", description = "상품 부정 키워드 조회")
+	@ApiResponse(responseCode = "200", description = "성공")
+	CommonResponse<ProductResponse.NegativeKeyword> getNegativeKeyword(
+			@Parameter(description = "상품 ID")
+			@PathVariable Integer id);
 }
--- src/main/java/spring/changyong/product/application/ProductAppService.java
+++ src/main/java/spring/changyong/product/application/ProductAppService.java
@@ -1,12 +1,30 @@
 package spring.changyong.product.application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import spring.changyong.common.api.code.ErrorCode;
+import spring.changyong.common.exception.BusinessLogicException;
 import spring.changyong.product.api.response.ProductResponse;
+import spring.changyong.product.domain.model.Product;
+import spring.changyong.product.domain.repository.ProductRepository;
 
 @Service
+@RequiredArgsConstructor
 public class ProductAppService {
+
+	private final ProductRepository productRepository;
+
 	public ProductResponse.Detail getProductDetail(Integer id) {
-		return null;
+		Product product = productRepository.findById(id).orElseThrow(() -> new BusinessLogicException(ErrorCode.NOT_FOUND_ENTITY, "상품을 찾을 수 없습니다."));
+
+		return ProductResponse.Detail.builder()
+				.id(product.getId().toString())
+				.title(product.getName())
+				.brand(product.getBrand())
+				.productId(product.getProductId())
+				.thumbnail(product.getThumbnail())
+				.keywords(null)
+				.build();
 	}
 
 	public ProductResponse.PositiveKeyword getPositiveKeyword(Integer id) {

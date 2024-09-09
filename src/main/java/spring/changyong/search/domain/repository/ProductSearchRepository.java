9829618b6689b00710092d6cf33688d93c676693
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-09-09 17:40:59 +0900

    [Feat] 검색 기능 개선

--- src/main/java/spring/changyong/search/domain/repository/ProductSearchRepository.java
+++ src/main/java/spring/changyong/search/domain/repository/ProductSearchRepository.java
@@ -1,16 +1,13 @@
 package spring.changyong.search.domain.repository;
 
-import co.elastic.clients.elasticsearch._types.SlicedScroll;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.core.SearchPage;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.repository.query.Param;
-import spring.changyong.product.domain.model.Product;
+import org.springframework.data.repository.CrudRepository;
 import spring.changyong.search.domain.model.ProductDocument;
 import spring.changyong.search.persistence.repository.CustomProductSearchRepository;
 
-public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, Long>, CustomProductSearchRepository {
-
-
+public interface ProductSearchRepository extends CustomProductSearchRepository, ElasticsearchRepository<ProductDocument, Long> {
+	Page<ProductDocument> searchByName(String name, Pageable pageable);
 }

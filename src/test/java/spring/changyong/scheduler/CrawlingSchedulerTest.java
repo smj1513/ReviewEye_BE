006756312e107a5d736be7e9c7aa8461bea7bedf
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-10-12 01:36:21 +0900

    스케쥴러 수정

---
+++ src/test/java/spring/changyong/scheduler/CrawlingSchedulerTest.java
@@ -0,0 +1,21 @@
+package spring.changyong.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Profile("dev")
+class CrawlingSchedulerTest {
+
+	@Autowired
+	private CrawlingScheduler crawlingScheduler;
+
+	@Test
+	void updatePrice() throws Exception {
+		crawlingScheduler.updatePrice();
+	}
+}

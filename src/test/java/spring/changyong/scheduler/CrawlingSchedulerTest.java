7075d2b41e0d07873bf0d1d2d7db613565d693fb
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-10-12 21:57:11 +0900

    테스트 수정

--- src/test/java/spring/changyong/scheduler/CrawlingSchedulerTest.java
+++ src/test/java/spring/changyong/scheduler/CrawlingSchedulerTest.java
@@ -1,5 +1,6 @@
 package spring.changyong.scheduler;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Disabled
 @Profile("dev")
 class CrawlingSchedulerTest {
 

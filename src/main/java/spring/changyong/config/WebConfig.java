b36336d3db8a8117e806d7ea71a0cc61b958152e
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-10-13 21:34:00 +0900

    Cors 설정

--- src/main/java/spring/changyong/config/WebConfig.java
+++ src/main/java/spring/changyong/config/WebConfig.java
@@ -7,14 +7,24 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableAspectJAutoProxy
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry){
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET","POST")
+				.allowCredentials(true);
 	}
 }

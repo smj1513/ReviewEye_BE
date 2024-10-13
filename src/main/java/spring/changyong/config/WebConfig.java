7a0ff32ab3da0d0d6315bd126ae67500c6933d8c
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-10-13 21:41:46 +0900

    Cors 설정 수정

--- src/main/java/spring/changyong/config/WebConfig.java
+++ src/main/java/spring/changyong/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry){
 		registry.addMapping("/**")
-				.allowedOrigins("*")
+				.allowedOriginPatterns("*")
 				.allowedMethods("GET","POST")
 				.allowCredentials(true);
 	}

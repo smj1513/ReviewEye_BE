172636a44acc7f9a50105bba0af57d9be5d5d87f
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-10-15 11:42:42 +0900

    스케쥴러 수정

--- src/main/java/spring/changyong/ChangyongApplication.java
+++ src/main/java/spring/changyong/ChangyongApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
 		type = FilterType.ASSIGNABLE_TYPE,
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 				spring.changyong.search.domain.repository.ReviewSearchRepository.class}
 ))
 @SpringBootApplication
+@EnableScheduling
 public class ChangyongApplication {
 
 	public static void main(String[] args) {

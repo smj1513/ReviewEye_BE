a0b0a04c1faad961933104a5616dfe2c707600d8
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2024-11-26 10:13:06 +0900

    스케쥴러 수정

--- src/main/java/spring/changyong/scheduler/CrawlingScheduler.java
+++ src/main/java/spring/changyong/scheduler/CrawlingScheduler.java
@@ -1,18 +1,14 @@
 package spring.changyong.scheduler;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import spring.changyong.common.api.code.ErrorCode;
-import spring.changyong.common.exception.BusinessLogicException;
 import spring.changyong.search.domain.model.ProductDocument;
 import spring.changyong.search.domain.repository.ProductSearchRepository;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @Log4j2
@@ -22,8 +18,9 @@ public class CrawlingScheduler {
 	private final ProductSearchRepository productSearchRepository;
 
 	private final CrawlingService crawlingService;
+
 	@Scheduled(cron = "0 0 3 * * ?")
-	public void updatePrice() throws Exception{
+	public void updatePrice() throws Exception {
 		int pageSize = 10;
 		int pageNumber = 0;
 		Page<ProductDocument> page;
@@ -39,7 +36,7 @@ public class CrawlingScheduler {
 				try {
 					Thread.sleep(500);
 				} catch (InterruptedException e) {
-					System.out.println(e.getMessage());
+					log.error(e.getMessage());
 				}
 			}
 
@@ -49,5 +46,9 @@ public class CrawlingScheduler {
 		} while (page.hasNext());
 
 		log.info("=========== All Pages Processing Complete ==========");
+	}
+	@PostConstruct
+	public void init(){
+		log.info("CrawlingScheduler initialized");
 	}
 }
